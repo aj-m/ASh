21d014e059a8e0bd0493bdbc819f2d55c3f26d40
Andrew Montenigro <andrew.montenigro@gmail.com>
2014-08-05 21:15:08 -0400

    Added project source files
    +ASh.dev
    +src\main.cpp
    +Makefile (maybe add to gitignore later)

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+/* Welcome to ASh!
+ * Name: ASh (A. Shell)
+ * Author: Andrew Montenigro
+ * Date: 08.04.2014 (August 4, 2014)
+ */
+
+
+
+
+#include <cstdlib>
+#include <conio.h>
+#include <windows.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Welcome to ASh!" << std::endl;
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
